28bc0cf54678c6262abd21ce9c75f845fe0cee7c
Wilbert Serrano <ad.wilbertserrano@ufg.edu.sv>
2020-11-26 05:39:21 -0600

    Prueba Formulario 4

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,10 @@
 let nombre = document.getElementById('nombre').value;
 let correo = document.getElementById('correo').value;
-let empresa = document.getElementById('nombre-empresa').value;
+let nombre_empresa = document.getElementById('nombre_empresa').value;
 let descripcion = document.getElementById('descripcion').value;
 emailjs.send("service_ff5436h","template_m0ias2f",{
     nombre: nombre,
     correo: correo,
-    empresa: empresa,
-    descripcion:descripcion,
+    empresa: nombre_empresa,
+    descripcion: descripcion,
     });
